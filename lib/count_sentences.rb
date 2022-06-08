779d9fe8cb061db78782a6ab5d56a614189f67d3
Eli Craig <elidcraig@gmail.com>
2022-06-07 21:31:16 -0700

    commit tree 6f2ed39569dd7f9c974e712f02461c4f4ef71559

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    split_array = self.split(' ')
+    select_array = split_array.select do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end  
+    select_array.length
   end
 end
+
+# binding.pry
